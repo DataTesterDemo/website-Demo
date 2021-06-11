465aedd3456c5002706eab2f0da2e62fdc82ba8e
DataTesterDemo <cheng.lam@bytedance.com>
2021-06-11 17:02:21 +0800

    Update formV2.js

--- src/components/Signup/formV2.js
+++ src/components/Signup/formV2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, Switch, Redirect} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 //========================================//
 //==========Sample form version 2=========//
