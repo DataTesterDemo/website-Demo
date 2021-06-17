d2a32a4ceed92ba2be13bab293aba3e4de5c53da
DataTesterDemo <cheng.lam@bytedance.com>
2021-06-17 14:20:40 +0800

    update promotion and firebase

--- src/components/Promotions/Promotion.js
+++ src/components/Promotions/Promotion.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link} from 'react-router-dom';
+import { Route, Link, Switch} from 'react-router-dom';
 import PromoBannerV1 from "./PromoBannerV1.js"
 import PromoBannerV2 from "./PromoBannerV2.js"
 
@@ -13,37 +13,38 @@ class Promotion extends React.Component{
     this.state = ({
       promotion: false
     })
+    console.log(this.state)
     this.onClick = this.onClick.bind(this)
   }
   onClick(){
+    console.log("te")
     this.setState({
       promotion: true
     })
   }
   //display content
   render(){
-    const {promotion} = this.state
-    if(!promotion){
-      return(
-        <div>
-          <h1>Promotion variations</h1>
-          <nav>
-            <ul>
-              <li><Link to="/promotion/v1" onClick={this.onClick}>Promotion V1</Link></li>
-              <li><Link to="/promotion/v2" onClick={this.onClick}>Promotion V2</Link></li>
-            </ul>
-          </nav>
-        </div>
-      )
-    }
     return(
       <div>
-        <Route path="/promotion/v1">
-          <PromoBannerV1/>
-        </Route>
-        <Route path="/promotion/v2">
-          <PromoBannerV2/>
-        </Route>
+        <Switch>
+          <Route path="/promotion/v1" >
+            <PromoBannerV1 />
+          </Route>
+          <Route path="/promotion/v2" >
+            <PromoBannerV2 />
+          </Route>
+          <Route path="/promotion/">
+            <div className="card">
+              <h1>Promotion variations</h1>
+              <nav>
+                <ul>
+                  <li><Link to="/promotion/v1" onClick={this.onClick}>Promotion V1</Link></li>
+                  <li><Link to="/promotion/v2" onClick={this.onClick}>Promotion V2</Link></li>
+                </ul>
+              </nav>
+            </div>
+          </Route>
+        </Switch>
       </div>
     )
   }
